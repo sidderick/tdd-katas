159d69145efec88d6e2ff8e2045b26040a4eee36
Sidd <davidsiddens@hotmail.co.uk>
2020-09-29 14:45:39 +0100

    testing greeter branch

---
+++ src/main/java/Greeter.java
@@ -0,0 +1,9 @@
+
+
+class Greeter {
+    public Object greet(String name) {
+        return "Hello " + name;
+    }
+
+
+}
--- src/test/java/GreeterTest.java
+++ src/test/java/GreeterTest.java
@@ -1,4 +1,12 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GreeterTest {
-  
+
+    @Test
+    void shouldGreetMe() {
+        assertEquals("Hello Bob", new Greeter().greet("Bob"));
+    }
+
 }
